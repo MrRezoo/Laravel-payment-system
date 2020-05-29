0e4b36a3324b779bfce161cc4af3c6c84a9ad0e8
Reza Mobaraki <rezam578@gmail.com>
2020-05-29 17:35:21 +0430

    event controller and index method and factory & seeder for faker data

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Event;
+use Faker\Generator as Faker;
+
+$factory->define(Event::class, function (Faker $faker) {
+    return [
+        'title' => $faker->randomElement([
+            'رویداد تکنولوژي',
+            'همایش ذهن برتر',
+            'رویداد علم داده',
+            'دورهمی برنامه نویسی',
+            'اجتماع و تکنولوژي',
+            'داده کاوی',
+            'معرفی اساتید برتر و ارائه تجربه',
+            'استارتاپ ویکند جم',
+            'استارتاپ پزشکی',
+            'استارتاپ ورزشی',
+            'رویداد ایده برتر',
+            'رویداد مدیریت زمان',
+        ]),
+        'description' => 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه .',
+        'image' => 'https://via.placeholder.com/286x180?text=Image',
+        'price' => $faker->randomElement([
+            150000 , 450000 , 252000 , 2521000 , 250000 , 150000 , 850000 , 650000, 450000 , 950000 , 410000 , 320000
+        ]),
+        'stock'=> $faker->randomDigitNotNull
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('events', 'EventController@index')->name('events.index');
+Route::get('basket/add/{event}', 'BasketController@add')->name('basket.add');
+Route::get('basket', 'BasketController@index')->name('basket.index');
+Route::post('basket/update/{event}', 'BasketController@update')->name('basket.update');
+Route::get('basket/checkout', 'BasketController@checkoutForm')->name('basket.checkout.form');
+Route::post('basket/checkout', 'BasketController@checkout')->name('basket.checkout');
+Route::post('payment/{gateway}/callback', 'PaymentController@verify')->name('payment.verify');
+
+Route::get('basket/clear', function () {
+    resolve(StorageInterface::class)->clear();
+});
+
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('auth.register.form');
+    Route::post('register', 'RegisterController@register')->name('auth.register');
+    Route::get('login', 'LoginController@showLoginForm')->name('auth.login.form');
+    Route::post('login', 'LoginController@login')->name('auth.login');
+    Route::get('logout', 'LoginController@logout')->name('auth.logout');
+    Route::get('email/send-verification', 'VerificationController@send')->name('auth.email.send.verification');
+    Route::get('email/verify', 'VerificationController@verify')->name('auth.email.verify');
+    Route::get('password/forget', 'ForgotPasswordController@showForgetForm')->name('auth.password.forget.form');
+    Route::post('password/forget', 'ForgotPasswordController@sendResetLink')->name('auth.password.forget');
+    Route::get('password/reset', 'ResetPasswordController@showResetForm')->name('auth.password.reset.form');
+    Route::post('password/reset', 'ResetPasswordController@reset')->name('auth.password.reset');
+    Route::get('redirect/{provider}', 'SocialController@redirectToProvider')->name('auth.login.provider.redirect');
+    Route::get('{provider}/callback', 'SocialController@providerCallback')->name('auth.login.provider.callback');
+    Route::get('magic/login', 'MagicController@showMagicForm')->name('auth.magic.login.form');
+    Route::post('magic/login', 'MagicController@sendToken')->name('auth.magic.send.token');
+    Route::get('magic/login/{token}', 'MagicController@login')->name('auth.magic.login');
+    Route::get('two-factor/toggle', 'TwoFactorController@showToggleForm')->name('auth.two.factor.toggle.form');
+    Route::get('two-factor/activate', 'TwoFactorController@activate')->name('auth.two.factor.activate');
+    Route::get('two-factor/code', 'TwoFactorController@showEnterCodeForm')->name('auth.two.factor.code.form');
+    Route::post('two-factor/code', 'TwoFactorController@confirmCode')->name('auth.two.factor.code');
+    Route::get('two-factor/deactivate', 'TwoFactorController@deactivate')->name('auth.two.factor.deactivate');
+    Route::get('login/code', 'LoginController@showCodeForm')->name('auth.login.code.form');
+    Route::post('login/code', 'LoginController@confirmCode')->name('auth.login.code');
+    Route::get('two-factor/resent', 'TwoFactorController@resent')->name('auth.two.factor.resent');
+});
+
